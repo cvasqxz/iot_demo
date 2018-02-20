51eb1c22ed79bf10f5806a9c60b8f08568342030
Cesar Vasquez <heellxz@gmail.com>
2018-02-20 15:46:05 -0300

    redondear a 2 decimales

--- __main__.py
+++ __main__.py
@@ -3,8 +3,8 @@ from time import sleep
 
 def main(sht):
 	while True:
-		temp = sht.read_t()
-		hum = sht.read_rh()
+		temp = round(sht.read_t(), 2)
+		hum = round(sht.read_rh(), 2)
 		print(temp, hum)
 		sleep(10)
 
