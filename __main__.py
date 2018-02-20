9af6ecee25bb1b443a7938e7b0de15f46e175cb5
Cesar Vasquez <heellxz@gmail.com>
2018-02-20 17:01:22 -0300

    prueba de 10 mediciones

--- __main__.py
+++ __main__.py
@@ -3,10 +3,20 @@ from time import sleep
 from struct import pack
 
 def main(sht):
+	buff_temp = buff_hum = ''
+	i = 0
+
 	while True:
-		temp = pack('f', round(sht.read_t(), 2))
-		hum = pack('f', round(sht.read_rh(), 2))
-		print(temp.encode('hex'), hum.encode('hex'))
+		buff_temp += pack('f', round(sht.read_t(), 2))
+		buff_hum += pack('f', round(sht.read_rh(), 2))
+		i += 1
+
+		if i == 10:
+			msg = buff_hum + buff_temp
+			print(msg, len(msg))
+			i = 0
+			buff_hum = buff_temp = ''
+
 		sleep(10)
 
 if __name__ == '__main__':
