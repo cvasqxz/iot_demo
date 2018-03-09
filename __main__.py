9fe6ec36d8c9eabed7f1225802af326db0cddc22
heellxz <heellxz@gmail.com>
2018-03-08 22:52:54 -0300

    comentarios

--- __main__.py
+++ __main__.py
@@ -11,14 +11,19 @@ def main(sht):
 	i = 0
 
 	while True:
+		# mediciones empaquetadas
 		temp = pack('f', round(sht.read_t(), 2))
 		hum = pack('f', round(sht.read_rh(), 2))
 		timestamp = pack('L', int(time()))
 
+		# agregar mediciones al buffer
 		msg += timestamp + temp + hum
 		i += 1
+
+		# debug
 		print('medicion %i: %s, %s' % (i, temp.encode('hex'), hum.encode('hex')))
 
+		# envio de tx
 		if i == 6:
 			result = OP_RETURN_send('cijgisFYuwYiqwhjZnbowrQYXsUq9G3GmA', mov, msg, False)
 
@@ -27,11 +32,15 @@ def main(sht):
 			else:
 				print('TxID: ' + result['txid'])
 
+			# reset
 			msg = ''
 			i = 0
 
 		sleep(100)
 
 if __name__ == '__main__':
+	# lector de sensor sht15
 	sht = Sht(24, 23)
+
+	# funcion principal
 	main(sht)
